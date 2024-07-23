f8e33e00db1a048a0fa2540185b91e6ea94e32d2
Paul Muñoz <paul18a20@gmail.com>
2024-07-22 22:26:36 -0500

    Subiendo cambios

--- gulpfile.js
+++ gulpfile.js
@@ -19,23 +19,23 @@ export function css( done ) {
     done()
 }
 
-export function html(done) {
-    src('index.html')
-        .pipe(dest('build'));
-    done();
-}
+// export function html(done) {
+//     src('index.html')
+//         .pipe(dest('build'));
+//     done();
+// }
 
 export function dev() {
     watch('src/scss/**/*.scss', css)
     watch('src/js/**/*.js', js)
-    watch('index.html', html);
+    // watch('index.html', html);
 }
 
 export function build( done ) {
     js(done);
     css(done);
-    html(done);
+    // html(done);
 }
 
 // export default series( js, css, dev )
-export default series(parallel(js, css, html), dev);
+export default series(parallel(js, css), dev);
