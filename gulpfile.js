e638f6e152bca50d3fea22103cf44ff0be486d67
Paul Muñoz <paul18a20@gmail.com>
2024-07-24 22:42:02 -0500

    Actualizando proyecto SASS

--- gulpfile.js
+++ gulpfile.js
@@ -27,23 +27,10 @@ export function css( done ) {
     done()
 }
 
-export function html(done) {
-    src('index.html')
-        .pipe(dest('build'));
-    done();
-}
-
 export function dev() {
     watch('src/scss/**/*.scss', css)
     watch('src/js/**/*.js', js)
-    watch('index.html', html);
-}
-
-export function build( done ) {
-    js(done);
-    css(done);
-    html(done);
 }
 
 // export default series( js, css, dev )
-export default series(parallel(js, css, html), dev);
+export default series(parallel(js, css), dev);
